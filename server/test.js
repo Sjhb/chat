b24b1c4b60aae07294f2783ce92798ddc394b330
Sjhb <1402204813@qq.com>
2017-03-28 15:53:16 +0800

    test.js cookie 操作

--- server/test.js
+++ server/test.js
@@ -1,17 +1,41 @@
 /**
  * Created by Manlin on 2017/2/19.
  */
-var fs=require('fs')
-var newDir = getHigher(__dirname);
-
-//取得上层路径
-function  getHigher(dir) {
-    var dirs=dir.split('/');
-    var newdir='';
-    for (var i = 0; i < dirs.length-2; i++) {
-        newdir += dirs[i] + '/';
+var cookieUtil={
+    get:function (name) {
+        var cookieValue=null;
+        //取出所有cookie
+        var cookies=document.cookie;
+        if (cookies.length>0){
+            cookieStart=cookies.indexOf(decodeURIComponent(name)+'=');
+            if(cookieStart!=-1){
+                cookieEnd=cookies.indexOf(';',cookieStart);
+                if (cookieEnd==-1){
+                    cookieEnd=cookies.length;
+                }
+            }
+            cookieValue=encodeURIComponent(cookies.substring(cookieStart+name.length+1,cookieEnd));
+        }
+        return cookieValue;
+    },
+    set:function (name,value,expires,path,domain,secure) {
+        var date=new Date();
+        var cookieText=decodeURIComponet(name)+'='+decodeRUIComponent(value);
+        if(expires instanceof Date){
+            cookieText+=';expires='+expires.toGMTString();
+        }
+        if(path){
+            cookieText+=';path='+path;
+        }
+        if(domain){
+            cookieText+=';domain='+dmain;
+        }
+        if(secure){
+            cookieText+=';secure'+secure;
+        }
+        document.cookie=cookieText;
+    },
+    delete:function (name,path,domain,secure) {
+        this.set(name,'',new Date(0),path,domain,secure)
     }
-    newdir+=dirs[dirs.length-2];
-    return newdir;
 }
-console.log(newDir);
